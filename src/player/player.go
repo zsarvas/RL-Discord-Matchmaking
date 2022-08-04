8be5e53fbc0c8ab863c53c8ab5deee76599eb160
Zach Sarvas <zachsarvas@yahoo.com>
2022-08-04 13:41:14 -0700

    minor changes to player struct

--- src/player/player.go
+++ src/player/player.go
@@ -1,16 +1,22 @@
 package player
 
+import (
+	"strings"
+)
+
 type player struct {
-	id        string
-	matchId   string
-	numWins   int
-	numLosses int
-	mmr       float32
-	inGame    bool
+	id          string
+	displayName string
+	matchId     string
+	numWins     int
+	numLosses   int
+	mmr         float32
+	inGame      bool
 }
 
 func newPlayer(id string) *player {
 
 	p := player{id: id}
+	p.displayName = strings.Split(id, "#")[0]
 	return &p
 }
